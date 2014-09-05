631b534903851f9b97f371756c26f48d474ce454
ethanchewy <ethanchewy@gmail.com>
2014-09-04 22:43:55 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -20,5 +20,17 @@ var main = function() {
       left: "0px"
     }, 200);
   });
+  
+  /*Opens about section*/
+  $('.about').click(function() {
+    $('.menu').animate({
+      left: "-285px"
+      
+    }, 200);
+    
+    $('body').animate({
+      left: "0px"
+    }, 200)
+  }
 };
 $(document).ready(main);
