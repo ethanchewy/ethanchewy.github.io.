9105d4da36aebfeb5417c4cf040f1da94e1cd538
ethanchewy <ethanchewy@gmail.com>
2014-09-10 20:08:46 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -35,6 +35,21 @@ var main = function() {
     $('.photo').animate({
       left: "512px"
     }, 200);
+    
+    $('#photo > img').each(function(){
+    //get img dimensions
+    var h = $(this).height();
+    var w = $(this).width();
+
+    //get div dimensions
+    var div_h =$('#photo').height();
+    var div_w =$('#photo').width();
+
+    //set img position
+    this.style.top = Math.round((div_h - h) / 2) + 'px';
+    this.style.left = '50%';
+    this.style.marginLeft = Math.round(w/2) + 'px';
+    });
   });
 };
 $(document).ready(main);
