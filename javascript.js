2ab3115d5a8124b213eb248e8f1d9a971ae99093
ethanchewy <ethanchewy@gmail.com>
2014-09-10 20:43:04 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -45,6 +45,7 @@ var main = function() {
   
   $('.facebook').click(function() {
     window.location.href="http://www.facebook.com/ethan.chiu.1420"; 
-  });
+  };
+  
 };
 $(document).ready(main);
