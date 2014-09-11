7b657e85b8354217b9596132009a4155699e5fb3
ethanchewy <ethanchewy@gmail.com>
2014-09-10 21:52:45 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -52,12 +52,6 @@ var main = function() {
   })
   
   /* Youtube */
-  .js-video embed, .js-video iframe, .js-video object, .js-video video {
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    position: absolute;
-  }
+
 };
 $(document).ready(main);
